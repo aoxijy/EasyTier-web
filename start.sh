289a9ff01e7ca0f2e40f66ad2a206cda7e64bda4
Cai <106552747+aoxijy@users.noreply.github.com>
2025-08-14 04:22:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,36 +1,35 @@
 #!/bin/sh
 set -e
 
-# 设置默认端口
+# 设置默认值
 CORE_PORT=${ET_CORE_PORT:-11010}
-API_PORT=${ET_API_PORT:-11211}
-CONFIG_PORT=${ET_CONFIG_PORT:-22020}
 WEB_PORT=${ET_WEB_PORT:-8080}
+NETWORK_NAME=${ET_NETWORK_NAME:-default}
+NETWORK_SECRET=${ET_NETWORK_SECRET:-}
 
 # API主机配置
 if [ -n "$ET_API_HOST" ]; then
   API_HOST="$ET_API_HOST"
 else
-  API_HOST="http://localhost:$API_PORT"
+  API_HOST="http://localhost:$CORE_PORT"  # 注意端口改为CORE_PORT
 fi
 
-# 启动核心服务 - 使用新的参数格式
+# 启动核心服务
 echo "启动 easytier-core..."
 /usr/local/bin/easytier-core \
   --rpc-portal "0.0.0.0:$CORE_PORT" \
+  --vpn-portal "0.0.0.0:$CORE_PORT" \
+  --network-name "$NETWORK_NAME" \
+  ${NETWORK_SECRET:+--network-secret "$NETWORK_SECRET"} \
   --listeners "tcp:$CORE_PORT" \
   --listeners "udp:$CORE_PORT" \
-  --listeners "wg:11011" \
-  --api-port "$API_PORT" \
-  --config-port "$CONFIG_PORT" &
+  --listeners "wg:11011" &
 
 # 启动Web服务
 echo "启动 easytier-web-embed..."
 /usr/local/bin/easytier-web-embed \
   --web-server-port "$WEB_PORT" \
-  --api-host "$API_HOST" \
-  --api-server-port "$API_PORT" \
-  --config-server-port "$CONFIG_PORT" &
+  --api-host "$API_HOST" &
 
 # 等待所有服务
 wait
